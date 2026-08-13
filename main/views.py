a76efa4a730d1e18cc3f0a3ead7df73c5d81e4bb
Ann Molochko <molochkoannav@gmail.com>
2026-08-13 20:56:25 +0300

    some changes

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request) -> HttpResponse:
-    return HttpResponse('Home page')
+    context = {"title": "Home page", "content": "Page content"}
+    return render(request, 'index.html', context)
 
 def about(request) -> HttpResponse:
     return HttpResponse('About page')
