16e4567c6e175efb0ceda80ed1d8a89d70e86a59
Ann Molochko <molochkoannav@gmail.com>
2026-08-13 19:44:47 +0300

    changes

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request) -> HttpResponse:
+    return HttpResponse('Home page')
+
+def about(request) -> HttpResponse:
+    return HttpResponse('About page')
+
